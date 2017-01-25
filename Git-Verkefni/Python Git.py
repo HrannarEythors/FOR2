71c45feed0d7e14e807f02dd8f558ef09c670707
Hrannar Örn Eyþórsson <hrannarey326@nemi.tskoli.is>
2017-01-25 13:41:15 +0000

    Gerði Dæmi 1

---
+++ Git-Verkefni/Python Git.py
@@ -0,0 +1,14 @@
+#Hrannar Örn Eyþórsonn
+
+print("<-----DÆMI 1----->")
+#Beðið er um tölur
+tala = int(input("Sláðu inn eina tölu "))
+tala2 = int(input("Sláðu inn aðra tölu "))
+#Reiknað margfeldi og summu
+margfeldi = tala*tala2
+suma = tala+tala2
+#Sýnt notanda niðurstöður
+print("\nSumma",tala,"og",tala2,"er =",suma)
+print("Margfeldi",tala,"og",tala2,"er =",margfeldi)
+
+
